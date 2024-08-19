99bfeb07fef2a8d3e210b1667ebe333417c8822f
NaniasNic <ananiasdias0@gmail.com>
2024-08-18 21:31:41 -0300

    refactor: nativewind reconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,30 @@
-import { colors } from "./src/styles/colors";
-import { fontFamily } from "./src/styles/font-family";
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
-      colors,
-      fontFamily,
+      colors: {
+        white: "#FDFCFB",
+
+        gray: {
+          900: "#202123",
+          800: "#2A2B2D",
+          700: "#323336",
+          600: "#444547",
+          400: "#9AA0A6",
+        },
+        orange: {
+          800: "#5E4D4E",
+          500: "#F28B81",
+          300: "#F6AEA9",
+        },
+        yellow: {
+          600: "#F5B400",
+        },
+        blue: {
+          600: "#4285F4",
+        },
+      },
     },
   },
   plugins: [],
